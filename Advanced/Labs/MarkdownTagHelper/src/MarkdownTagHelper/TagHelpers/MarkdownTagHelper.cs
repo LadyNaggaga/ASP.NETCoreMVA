acecbceffe3d83d157aa8ecf9a00eff894054630
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2016-09-20 05:45:22 -0700

    Added MarkdownTagHelper sample

---
+++ Advanced/Labs/MarkdownTagHelper/src/MarkdownTagHelper/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using CommonMark;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkdownTagHelper.TagHelpers
+{
+  [HtmlTargetElement("markdown")]
+  [HtmlTargetElement(Attributes = "markdown")]
+  public class MarkdownTagHelper : TagHelper
+  {
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+      output.TagName = null;
+
+      var childContent = await output.GetChildContentAsync();
+      var lines = childContent.GetContent()
+          .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+          .Select(line => line.Trim());
+      var content = string.Join(" ", lines);
+      var transformedContent = CommonMarkConverter.Convert(content);
+
+      output.Content.SetHtmlContent(transformedContent);
+
+      output.Attributes.RemoveAll("markdown");
+    }
+  }
+}
